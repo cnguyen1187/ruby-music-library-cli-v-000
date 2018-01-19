a4e0345619dc68dc2048ee179d03b4d7475efd36
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-19 21:35:09 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,19 +1,15 @@
-require 'pry'
-
 class MusicImporter
   attr_accessor :path
 
   def initialize(path)
-    @path = path
+    self.path = path
   end
 
   def files
-    Dir.glob("#{path}/*.mp3").collect{|file| file.gsub("#{path}/", "")}
+    Dir.entries(self.path).delete_if{|s| s.length < 3}
   end
 
   def import
-    files = Dir.glob("#{path}/*.mp3").collect{|file| file.gsub("#{path}/", "")}
-    files.each{|f| Song.create_from_filename(f)}
+    files.each {|s| Song.create_from_filename(s)}
   end
-
 end
